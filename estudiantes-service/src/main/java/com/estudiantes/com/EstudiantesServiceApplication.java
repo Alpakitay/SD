968eb41f75552302084c5eb0720f8f1baf96643c
Th3Amiguit0 <the4miguito202@gmail.com>
2023-07-26 12:53:03 -0500

    eureka

--- estudiantes-service/src/main/java/com/estudiantes/com/EstudiantesServiceApplication.java
+++ estudiantes-service/src/main/java/com/estudiantes/com/EstudiantesServiceApplication.java
@@ -2,10 +2,12 @@ package com.estudiantes.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EstudiantesServiceApplication {
 
 	public static void main(String[] args) {
--- modalidad-service/src/main/java/com/modalidadservice/com/ModalidadServiceApplication.java
+++ modalidad-service/src/main/java/com/modalidadservice/com/ModalidadServiceApplication.java
@@ -2,8 +2,10 @@ package com.modalidadservice.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ModalidadServiceApplication {
 
 	public static void main(String[] args) {
